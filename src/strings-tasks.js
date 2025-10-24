7d82b84e2407e6206bbea8592674553003ceacb0
Karina <karina2006.ka@gmail.com>
2025-10-24 17:49:47 +0300

    feat: add concat

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -53,8 +53,9 @@ function isString(/* value */) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  console.log(value1, value2);
+  return value1.concat(value2);
 }
 
 /**
